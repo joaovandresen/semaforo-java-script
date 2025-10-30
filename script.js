8ae33ef9eacd91aa66a3400b60034dfed9450732
João Victor <joao.vandresen.costa@escola.pr.gov.br>
2025-10-30 14:51:06 +0000

    js

--- script.js
+++ script.js
@@ -3,4 +3,31 @@ const luzVermelha = document.getElementById("vermelho");
 const luzAmarela = document.getElementById("amarela");
 const luzVerde = document.getElementById("verde");
 // Botões
-const btnVermelho = document.getElementById
+const btnVermelho = document.getElementById("btnVermelho");
+const btnAmarelo = document.getElementById("btnAmarelo");
+const btnVerde = document.getElementById("btnVerde");
+const btnAuto = document.getElementById("btnAuto");
+const btnParar = document.getElementById("btnParar");
+// Variável para o modo automático
+let intervalo;
+
+function limpar(){
+    luzVermelha.className = 'luz';
+    luzAmarela.className = 'luz';
+    luzVerde.className = 'luz';
+}
+
+function acenderVermelho(){
+    limpar();
+    luzVermelha.classList.add("acesa","vermelha");   
+}
+
+function acenderAmarelo(){
+    limpar();
+    luzAmarelo.classList.add("acesa","amarelo");       
+}
+
+function acenderVerde(){
+    limpar();
+    luzVerde.classList.add("acesa","verde");       
+}
