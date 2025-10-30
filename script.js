09dfb36cf0b97659f80567b2d84e22b857c13b51
João Victor <joao.vandresen.costa@escola.pr.gov.br>
2025-10-30 14:38:23 +0000

    js

---
+++ script.js
@@ -0,0 +1,6 @@
+// Selecione os elementos das luzes
+const luzVermelha = document.getElementById("vermelho");
+const luzAmarela = document.getElementById("amarela");
+const luzVerde = document.getElementById("verde");
+// Botões
+const btnVermelho = document.getElementById
